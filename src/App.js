1e3bf3f05fb4731dc8c7a60e874474eded680f04
Ren Tung <rencodingwork@gmail.com>
2022-09-21 19:37:01 +0800

    帶入CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from "react";
+import { useEffect } from "react";
+
+import './App.css'
+import SearchIcon from './search.svg'
+
+//15d31df3
+
+const API_URL = 'http://www.omdbapi.com?apikey=15d31df3';
+
+const movie1 = {
+    "Title": "Superman, Spiderman or Batman",
+    "Year": "2011",
+    "imdbID": "tt2084949",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+}
 
 const App = () => {
+
+    const searchMovies = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+
+    }
+
+    useEffect(()=>{
+        searchMovies('Spiderman')
+    },[])
+
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>冬冬影城</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="搜尋電影(英文名稱)"
+                    value="Spider"
+                    onChange={()=>{}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={()=>{}}
+                />
+            </div>
+            <div></div>
+        </div>  
     );
 }
 
