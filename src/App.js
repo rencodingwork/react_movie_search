d057e0fc453127bfb3ed2feab14502221bef28eb
Ren Tung <rencodingwork@gmail.com>
2022-09-21 20:14:29 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import './App.css'
 import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
 
 //15d31df3
 
 const API_URL = 'http://www.omdbapi.com?apikey=15d31df3';
 
-const movie1 = {
-    "Title": "Superman, Spiderman or Batman",
-    "Year": "2011",
-    "imdbID": "tt2084949",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-}
-
 const App = () => {
+
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+
 
     const searchMovies = async (title) =>{
         const response = await fetch(`${API_URL}&s=${title}`)
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
 
     }
 
     useEffect(()=>{
-        searchMovies('Spiderman')
+        searchMovies('Iron man')
     },[])
 
     return (
@@ -36,16 +33,30 @@ const App = () => {
             <div className="search">
                 <input 
                     placeholder="搜尋電影(英文名稱)"
-                    value="Spider"
-                    onChange={()=>{}}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <img 
                     src={SearchIcon}
                     alt="search"
-                    onClick={()=>{}}
+                    onClick={()=>searchMovies(searchTerm)}
                 />
             </div>
-            <div></div>
+
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie)=>(
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>找不到此電影</h2>
+                    </div>
+                )}
+
+           
         </div>  
     );
 }
